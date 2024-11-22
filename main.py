c74f7fd16b0a87d0326fbea147595d7ed3e226ed
tutakamonb <tutankamonb@gmail.com>
2024-11-21 21:01:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import datetime
+
+"""Atributos de un experimento: nombre, fecha(DD/MM/AAAA), tipo y resultados numericos"""
+lis8taDeExperimentos = [
+  ["Experimento 1", "16/11/2024", "Quimica", [5,3,6,4,7,9]],
+]
+
+def agregar_experimento():
+ """permite agregar un nuevo experimento con sus atributos"""
+ pass
+
+def eliminar_experimento():
+  """permite eliminar un experimento"""
+  pass
+
+def visualizar_experimento():
+  """Permite vizualizar todos los experimentos"""
+  pass
+
+def calcular_estadisiticas():
+  """Calcular estadisticas basicas(promedios, maximos y minimos)"""
+  pass
+
+def comparar_experimentos():
+  """compara dos o mas experimentos para verificar cuales son los mejores resultados"""
+  pass
+def generar_informe():
+  """Generar informe resumen de los experimentos"""
+  pass
+
+def mostrar_menu():
+  """muestra el menu principal del proyecto"""
+  print("=========Menu Principal========")
+  print("1. Agregar Experimento")
+  print("2. Visualizar Experimento")
+  print("3. Eliminar Experimento")
+  print("4. Calcular estadisticas")
+  print("5. Comparar Experimento")
+  print("6. Generar Informe")
+  print("7. Salir")
+
+  pass
+
+def main():
+  """Controla el flujo general del sistema"""
+  mostrar_menu()
+  pass
+
+
+main()
